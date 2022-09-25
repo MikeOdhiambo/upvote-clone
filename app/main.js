4814c6846a33560ad3654b54b77f2e14c29dbc5f
MikeOdhiambo <odhiambomike413@gmail.com>
2022-09-26 00:05:16 +0300

    Create card component and make props

--- app/main.js
+++ app/main.js
@@ -1,23 +1,46 @@
+const submissionComponent = {
+  template: `<div class="card">
+				<div class="card__dets">
+					<img alt="Avatar1" v-bind:src="submission.avatar">
+					<p>{{ submission.name }}</p>
+				</div>
+				<div class="card__post">
+					{{ submission.post }}
+				</div>
+				<div class="card__likes">
+					<span v-on:click="upvote(submission.id)">
+						<i class="fa-regular fa-thumbs-up"></i>
+						<strong class="card__likes-num">
+							{{ submission.votes }}
+						</strong>
+					</span>
+				</div>
+			</div>`,
+	props: ["submission", "submissions"],			
+};
 const upvoteApp = {
-	data() {
-		return {
-			submissions: Seed.submissions
-		}
-	},
-	computed: {
-		sortedSubmissions() {
-			return this.submissions.sort((a,b) => {
-				return b.votes - a.votes;
-			});
-		},
-	},
-	methods: {
-		upvote(submissionId) {
-			const submission = this.submissions.find(
-				(submission) => submission.id === submissionId
-			);
-			submission.votes++;
-		},
-	},
+  data() {
+    return {
+      submissions: Seed.submissions,
+    };
+  },
+  computed: {
+    sortedSubmissions() {
+      return this.submissions.sort((a, b) => {
+        return b.votes - a.votes;
+      });
+    },
+  },
+  methods: {
+    upvote(submissionId) {
+      const submission = this.submissions.find(
+        (submission) => submission.id === submissionId
+      );
+      submission.votes++;
+    },
+  },
+  components: {
+    "submission-component": submissionComponent,
+  },
 };
 Vue.createApp(upvoteApp).mount('#app')
